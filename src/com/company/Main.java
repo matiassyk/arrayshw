9f11b58c02cd7f8ed8b4d2d51b809d0203ed14f8
matiasmettala <uh@sjkksd.com>
2021-09-15 10:59:40 +0300

    m

---
+++ src/com/company/Main.java
@@ -0,0 +1,89 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //#1
+//        int[] numbers = {2, 5, -3, 11, 193, -2};
+//        int sum = 0;
+//        for (int i = 0; i < numbers.length; i++)
+//        {
+//            sum = sum + numbers[i];
+//            System.out.println(i);
+//        }
+//        System.out.println(sum);
+
+        //#2
+//        int[] nums = {3, 5, 0, 11,1};
+//        int count = 0;
+//        int s = 0;
+//
+//        for (int i = 0; i <= 4 && i>= 0; i++)
+//        {
+//            if (!(nums[i] == 0))
+//            {
+//                s = s + nums[i];
+//                count++;
+//            }
+//        }
+//        if (count == 0)
+//            System.out.println(nums[0]);
+//        else
+//            System.out.println(s % count);
+
+        //#3
+//        int[] values = {-3, 2, 5, 4, 7};
+//        int n = 1;
+//        boolean result = true;
+//
+//        for (result = true; n < 5; n++)
+//            if (values[n] < values[n-1])
+//            {
+//                result = false;
+//                n= n++;
+//            }
+//        else
+//            n = n++;
+//        System.out.println(result);
+
+        //#4
+//        int[] arr = {5, 4, -7, -2, -1};
+//
+//        for (int i = arr.length-1; i >= 0; i--)
+//            System.out.print(arr[i] + " ");
+
+        //#5
+//        int[] numbers = {-3, 2, 6, -11};
+//        int sum = 0;
+//        for (int i = 0; i < numbers.length; i++)
+//            {
+//                sum += numbers[i];
+//                System.out.println(sum + " ");
+//            }
+//
+
+        //#6??
+//        int[] arr = {4, -3, -5, 9, 1, 11};
+//        int m = 1;
+//        int[] bigger = new int[arr.length];
+//        int[] smaller = new int[arr.length];
+//
+//        for (int i = 0; i < arr.length; i++)
+//            if (m > arr[i])
+//            {
+//                arr[i] = bigger[];
+//                System.out.println(bigger);
+//            }
+//            if (m < arr[i])
+//            {
+//                arr[i] = smaller[]
+//                System.out.println(smaller);
+//            }
+
+
+
+
+
+        }
+}
